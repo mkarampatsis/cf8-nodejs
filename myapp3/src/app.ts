1c1fa1f9e3cc55d97d9c0d1b26fbe61f355083df
mkarampatsis <marka@central.ntua.gr>
2025-10-28 05:50:10 +0200

    Myapp3

--- myapp3/src/app.ts
+++ myapp3/src/app.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import { greet } from "./utils";
 
 const app = express()
 
 app.get('/', (req, res)=>{
   res.send("Hello World");
+});
+
+app.get('/greetings', (req, res)=>{
+  const message = greet("Markos");
+  res.send(message);
 })
 
 app.listen(3000, ()=>{
   return console.log("Server is up");
-})
+});
